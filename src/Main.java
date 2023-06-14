89fa698e141a5ddc062d8ec045ac5925510ed579
Yuriy S <dev@solovyov.de>
2023-06-14 14:07:36 +0200

    initial project

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void add(int num1, int num2) {
+        System.out.println(num1 + num2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
